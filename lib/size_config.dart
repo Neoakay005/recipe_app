1020ed46238480d136f1c24747e48ae03c76fc48
Neoakay005 <akayhalil0505@gmail.com>
2023-11-25 16:20:25 +0300

    entry2

--- lib/size_config.dart
+++ lib/size_config.dart
@@ -6,4 +6,16 @@ class SizeConfig {
   static double screenHeight;
   static double defaultSize;
   static Orientation orientation;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    orientation = _mediaQueryData.orientation;
+    // On iPhone 11 the defaultSize = 10 almost
+    // So if the screen size increse or decrease then our defaultSize also vary
+    defaultSize = orientation == Orientation.landscape
+        ? screenHeight * 0.024
+        : screenWidth * 0.024;
+  }
 }
